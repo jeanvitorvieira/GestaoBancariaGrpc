8fcf03918492735020e1d86fbc7439bb14ec9edf
jeanvitorvieira <jeanvitorv0@gmail.com>
2026-04-20 17:09:16 -0300

    Ajustando projeto gRPC

--- Apresentacao/Program.cs
+++ Apresentacao/Program.cs
@@ -1,14 +1,16 @@
+using Dominio.Infra;
 using Apresentacao.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+ServicesExtensionDomain.ConfigurarDominio(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<ContasBancariasService>();
+app.MapGrpcReflectionService();
 
 app.Run();
--- Repositorio/Migrations/DataContextModelSnapshot.cs
+++ Repositorio/Migrations/DataContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace Repositorio.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "8.0.25");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
 
             modelBuilder.Entity("Entidades.ContaBancaria", b =>
                 {
